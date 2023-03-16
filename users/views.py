e0d25b1a5e596043b1027b8d83570fb7f7f62902
zTMXz <erekhovets200226@gmail.com>
2023-03-16 13:06:36 +0300

    reg mail

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate, login
+from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render, redirect
 from django.views import View
 
+from EV_chargers.settings import DEFAULT_FROM_EMAIL
 from users.forms import UserCreationForm
 
 
@@ -22,9 +24,19 @@ class Register(View):
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
             user = authenticate(username=username, password=password)
             login(request, user)
+
+            #РАБОТАЕТ, ПОТОМ ПЕРЕПИСАТЬ
+            send_mail(subject='registration',
+                      message='thx for reg',
+                      recipient_list=[email],
+                      from_email=DEFAULT_FROM_EMAIL
+                      )
+
             return redirect('home')
+
 
 
         context = {
