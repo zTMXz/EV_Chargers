63009a817b9d463f50874c261bb1d554687639f8
zTMXz <erekhovets200226@gmail.com>
2023-03-22 22:01:35 +0300

    detect user current location

--- google_maps_ev/static/google_maps_ev/js/search_control_ppo.js
+++ google_maps_ev/static/google_maps_ev/js/search_control_ppo.js
@@ -1,33 +1,37 @@
 function init() {
-    var myMap = new ymaps.Map('map', {
-        center: [53.902292, 27.561821],
-        zoom: 13,
-        controls: []
-    });
-    
-    // Создадим экземпляр элемента управления «поиск по карте»
-    // с установленной опцией провайдера данных для поиска по организациям.
-    var searchControl = new ymaps.control.SearchControl({
-        options: {
-            provider: 'yandex#search'
-        }
-    });
-    
-    myMap.controls.add(searchControl);
-    
-    // Программно выполним поиск определённых кафе в текущей
-    // прямоугольной области карты.
-    searchControl.search('Электрозаправка');
+    // Получаем координаты пользователя
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var userLat = position.coords.latitude;
+        var userLng = position.coords.longitude;
 
-    // Подписывается на событие получения результатов поиска с сервера.
-    searchControl.events.add('load', function (e) {
-        var count = searchControl.getResultsCount();
-        console.log("Количество найденных результатов поиска: " + count);
-    })
+        // Создаем карту с координатами пользователя
+        var myMap = new ymaps.Map('map', {
+            center: [userLat, userLng],
+            zoom: 15,
+            controls: []
+        });
 
+        // Создадим экземпляр элемента управления «поиск по карте»
+        // с установленной опцией провайдера данных для поиска по организациям.
+        var searchControl = new ymaps.control.SearchControl({
+            options: {
+                provider: 'yandex#search',
+                noPopup: true
+            }
+        });
+
+        myMap.controls.add(searchControl);
+
+        // Программно выполним поиск определённых кафе в текущей
+        // прямоугольной области карты.
+        searchControl.search('Электрозаправка');
+
+        // Подписывается на событие получения результатов поиска с сервера.
+        searchControl.events.add('load', function (e) {
+            var count = searchControl.getResultsCount();
+            console.log("Количество найденных результатов поиска: " + count);
+        })
+    });
 }
 
-
-
 ymaps.ready(init);
-
