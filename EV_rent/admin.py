a0b6be8c7391d3d696194b4915fa53187f3b265b
zTMXz <erekhovets200226@gmail.com>
2023-03-31 02:58:13 +0300

    add new classes

--- EV_rent/admin.py
+++ EV_rent/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Car, ServiceRequest, RentalRequest
+from .models import Car, ServiceRequest, RentalRequest, RentalHistory, ServiceHistory
 
 admin.site.register(Car)
 admin.site.register(ServiceRequest)
 admin.site.register(RentalRequest)
+admin.site.register(RentalHistory)
+admin.site.register(ServiceHistory)
