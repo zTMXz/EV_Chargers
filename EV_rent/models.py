971bcecf337e5677b225ca194720f5afda8ffcb3
zTMXz <erekhovets200226@gmail.com>
2023-03-31 02:59:55 +0300

    new realization of Rent and Service, add 2 new classes

--- EV_rent/models.py
+++ EV_rent/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from datetime import datetime
+
+from users.models import User
 
 
 # Электромобили (ид, Модель, Тип зарядки, кол-во л.с., сидячих мест, цена аренды)
 
 class Car(models.Model):
+    car_full_name = models.CharField(max_length=100)
     car_brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     charge_type = models.CharField(max_length=100)
@@ -27,27 +33,53 @@ class Car(models.Model):
 
 
 class ServiceRequest(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    car_model = models.CharField(max_length=100)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    car_model = models.ForeignKey(Car, on_delete=models.CASCADE)
     description = models.TextField()
     photos = models.ImageField(upload_to='repair_images/%Y/%m/%d/', blank=True)
-    is_broken = models.BooleanField(default=False)
-    is_rented = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.person.first_name} {self.person.last_name} сообщил о поломке {self.car_model}"
 
 
 class RentalRequest(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    car_model = models.CharField(max_length=100)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    car_model = models.ForeignKey(Car, on_delete=models.CASCADE)
     num_days = models.IntegerField()
-    is_broken = models.BooleanField(default=False)
-    is_rented = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.car_model}"
+        return f"{self.person.first_name} {self.person.last_name} арендовал {self.car_model}"
+
+class RentalHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(default=datetime.now, blank=True)
+    end_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}, аренда {self.car} от {self.start_date.strftime('%Y-%m-%d')}"
+class ServiceHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}, поломка {self.car} от {self.date.strftime('%Y-%m-%d')}"
+
+
+
+@receiver(pre_delete, sender=RentalRequest)
+def set_car_rent_as_available(sender, instance, **kwargs):
+    car = instance.car_model
+    car.is_rented = False
+    car.save()
+
+
+@receiver(pre_delete, sender=ServiceRequest)
+def set_car_broken_as_available(sender, instance, **kwargs):
+    car = instance.car_model
+    car.is_broken = False
+    car.save()
+
+
