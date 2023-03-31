9b82ad5d699e024084d47e3314713729cfebc9a4
zTMXz <erekhovets200226@gmail.com>
2023-03-31 03:01:43 +0300

    upd

--- EV_rent/urls.py
+++ EV_rent/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.CarList.as_view(), name='rent'),
     path('service_request/', views.service_request, name='service_request'),
-    path('rental_request/', views.rental_request, name='rental_request'),
+    path('rental_request/<int:car_id>', views.rental_request, name='rental_request'),
     path('service_request_success/', views.service_request_success, name='service_request_success'),
     path('<slug:slug>/', views.ModelDetailView.as_view(), name='model_info'),
 ]
