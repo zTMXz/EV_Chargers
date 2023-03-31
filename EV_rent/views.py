378f115bbc5f578b72b7358298d19f81b6744d27
zTMXz <erekhovets200226@gmail.com>
2023-03-31 03:02:02 +0300

    new realization of Rent and Service

--- EV_rent/views.py
+++ EV_rent/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Car, ServiceRequest, RentalRequest
+from .models import Car, ServiceRequest, RentalRequest, RentalHistory, ServiceHistory
 from .forms import ServiceRequestForm, RentalRequestForm
-from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
+from users.models import User
+
+from datetime import datetime, timedelta
 
 
 class CarList(ListView):
@@ -20,13 +22,18 @@ class ModelDetailView(DetailView):
 def service_request(request):
     if request.method == 'POST':
         form = ServiceRequestForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
 
-            model = " ".join(str(form.cleaned_data.get('car_model')).split()[1:])
-            car = Car.objects.get(model=model)
+        if form.is_valid():
+            service_request = form.save(commit=False)  # Создаем объект, но не сохраняем его в БД
+            service_request.person = request.user  # Указываем пользователя в поле person
+            service_request.save()
+
+            car_model = service_request.car_model
+            car = Car.objects.get(car_full_name=car_model)
             car.is_broken = True
             car.save()
+
+            ServiceHistory.objects.create(user=request.user, car=car, description=service_request.description)
 
             return redirect('service_request_success')
     else:
@@ -38,24 +45,28 @@ def service_request_success(request):
     return render(request, 'EV_rent/service_request_success.html')
 
 
-def rental_request(request):
+def rental_request(request, car_id):
+    car = Car.objects.get(pk=car_id)
     if request.method == 'POST':
         form = RentalRequestForm(request.POST)
-        if form.is_valid():
-            form.save()
 
-            model = " ".join(str(form.cleaned_data.get('car_model')).split()[1:])
-            rent = RentalRequest.objects.get(car_model=form.cleaned_data.get('car_model'))
-            car = Car.objects.get(model=model)
+        if form.is_valid():
+            rental_request = form.save(commit=False)  # Создаем объект, но не сохраняем его в БД
+            rental_request.person = request.user  # Указываем пользователя в поле person
+
+
+            rental_request.car_model = car
+            rental_request.save()
+
             car.is_rented = True
-            rent.is_rented = True
             car.save()
-            rent.save()
+
+            RentalHistory.objects.create(user=request.user, car=car, start_date=datetime.now(), end_date=(datetime.now() + timedelta(days=rental_request.num_days)))
 
             return redirect('service_request_success')
     else:
         form = RentalRequestForm()
-    return render(request, 'EV_rent/rental_request.html', {'form': form})
+    return render(request, 'EV_rent/rental_request.html', {'form': form, 'car': car})
 
 
 def rental_request_success(request):
